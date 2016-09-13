2741bd44015d1110a2b4bdc77d9740196dec1642
Christopher Barry <me@chrisb.xyz>
2016-09-12 21:20:04 -0400

    Force https on Tumblr images

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		var randNum = Math.round(Math.random() * (tumblr_api_read.posts.length - 1));
 		var recentPhoto = tumblr_api_read.posts[randNum];
 		console.log();
-		var photoUrl = recentPhoto['photo-url-1280'];
+		var photoUrl = recentPhoto['photo-url-1280'].replace("http://", "https://");
 		var postUrl = recentPhoto.url;
 		var caption = $(recentPhoto['photo-caption']).text();
 		$('head').append('<style>.left-side:after{background-image: linear-gradient(102deg, rgba(0, 195, 216, 0.82) 9%, rgba(186, 39, 66, 0.51) 100%), url("' + photoUrl + '")}</style>');
