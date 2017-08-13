4fa49522b0647243a64e7874dc3a55b1eb09fcec
Chris Barry <me@chrisb.xyz>
2017-08-13 12:34:44 -0400

    url fix

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.use(bodyParser.json());
 app.use(compression());
 app.use(scribe.express.logger(console)); // Log each request
 app.use('/logs', scribe.webPanel());
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('pages/index');
 });
-app.get('/twitter', function(req, res) {
+app.get('/twitter', function (req, res) {
   console.log('getting recent tweet');
   const params = {
     screen_name: 'ChrisW_B',
@@ -49,7 +49,7 @@ app.get('/twitter', function(req, res) {
     exclude_replies: true,
     include_rts: false
   };
-  twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+  twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
       const newTweet = tweets[0];
       res.send({
@@ -69,9 +69,9 @@ app.get('/twitter', function(req, res) {
   });
 });
 
-app.get('/bg', function(req, res) {
-  request(`https://photo.chriswbarry.com/ghost/api/v0.1/posts?client_id=${config.ghost.id}&client_secret=${config.ghost.secret}&fields=feature_image,url,title&limit=7`,
-    function(error, result, json) {
+app.get('/bg', function (req, res) {
+  request(`https://photo.chriswbarry.com/ghost/api/v0.1/posts?client_id=${config.ghost.id}&client_secret=${config.ghost.secret}&limit=7&fields=feature_image,url,title`,
+    function (error, result, json) {
       if (!error && result.statusCode === 200) {
         const posts = JSON.parse(json).posts;
         const randNum = Math.round(Math.random() * (posts.length - 1));
@@ -88,8 +88,8 @@ app.get('/bg', function(req, res) {
   );
 });
 
-app.get('/bginfo', function(req, res) {
-  setTimeout(function() {
+app.get('/bginfo', function (req, res) {
+  setTimeout(function () {
     if (recentPhoto !== undefined && recentPhoto !== '') {
       res.send({
         success: true,
@@ -104,7 +104,7 @@ app.get('/bginfo', function(req, res) {
   }, 1000); // give /bg time to get a photo
 });
 
-app.get('/lastfm', function(req, res) {
+app.get('/lastfm', function (req, res) {
   console.log('getting recent play');
   lastFmClient.user_getRecentTracks({
     user: 'Christo27',
@@ -125,7 +125,7 @@ app.get('/lastfm', function(req, res) {
   });
 });
 
-app.listen(4737, function() {
+app.listen(4737, function () {
   console.log('Running on port 4737!');
 });
 
