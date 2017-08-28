42d1d20b7043c8d867267fdd50816f6020441bfc
Chris Barry <me@chrisb.xyz>
2017-08-28 12:01:42 -0400

    bit more logs

--- server/index.js
+++ server/index.js
@@ -155,10 +155,11 @@ const ensureGithub = (req, res, next) => {
     console.log('no ua')
     res.redirect(301, '/');
   }
+  logger.server({ env: process.env })
   const hmac = crypto.createHmac('sha1', process.env.SECRET_TOKEN);
   const ourSignature = `sha1=${hmac.update(JSON.stringify(req.body)).digest('hex')}`;
   const theirSignature = req.get('X-Hub-Signature');
-  logger.server({ token: process.env.SECRET_TOKEN, ourSignature, theirSignature })
+  logger.server({ ourSignature, theirSignature })
   if (crypto.timingSafeEqual(Buffer.from(ourSignature, 'utf8'), Buffer.from(theirSignature, 'utf8'))) return next();
   else {
     console.log('no match');
