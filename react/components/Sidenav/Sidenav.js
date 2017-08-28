e06b1dd44ba4e70faae3c7ad3f311cf38353b8a5
Chris Barry <me@chrisb.xyz>
2017-08-28 13:23:42 -0400

    bye keybase II

--- react/components/Sidenav/Sidenav.js
+++ react/components/Sidenav/Sidenav.js
@@ -14,7 +14,7 @@ export default class Sidenav extends Component {
   sidenavElements = [
     { link: 'mailto:me@chriswbarry.com', title: 'Get in Touch!' },
     { link: '//github.com/ChrisW-B/', title: 'Github' },
-    { link: '//keybase.io/chriswb', title: 'Keybase' },
+
     { link: '//linkedin.com/in/chriswbarry', title: 'LinkedIn' },
     { link: '//photo.chriswbarry.com/', title: 'Photography' }
   ]
