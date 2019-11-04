a0501e10befc2612cb90d7da763e3197bb615aa7
Chris Barry <me@chrisb.xyz>
2019-11-04 08:08:28 -0500

    feat: pull profile photo from backblaze

--- src/App/components/Content/index.js
+++ src/App/components/Content/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { useQuery } from 'react-apollo';
 
 import DetailSections from '@components/Content/DetailSections';
 import Intro from '@components/Content/Intro';
 import LearnMore from '@components/Content/LearnMore';
-import ProfilePic from '@public/images/prof.jpg';
+import profilePhotoQuery from '@queries/profilePhotoQuery.gql';
 import { InfoSection, ProfilePhoto } from '@styles/Info';
 
-const Content = () => (
-  <InfoSection>
-    <ProfilePhoto src={ProfilePic} alt='Profile' />
-    <Intro />
-    <DetailSections />
-    <LearnMore />
-  </InfoSection>
-);
+const Content = () => {
+  const { data, loading, error } = useQuery(profilePhotoQuery);
+  return (
+    <InfoSection>
+      <ProfilePhoto src={!loading && !error && data.photo} alt='Profile' />
+      <Intro />
+      <DetailSections />
+      <LearnMore />
+    </InfoSection>
+  );
+};
 
 export default Content;
