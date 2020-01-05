2179baea3161c427a31a6e77351b2c23c7f15356
Chris Barry <me@chrisb.xyz>
2020-01-05 14:41:09 -0500

    chore: fix up typings, clean up tsconfig

--- src/App/typings/index.d.ts
+++ src/App/typings/index.d.ts
@@ -3,14 +3,4 @@ declare global {
     __APOLLO_STATE__: any;
   }
 }
-declare module '*.gql' {
-  import graphql = require('graphql');
-  const content: any;
-  export default content;
-}
-declare module '*.graphql' {
-  import graphql = require('graphql');
-  const content: any;
-  export default content;
-}
 export {};
